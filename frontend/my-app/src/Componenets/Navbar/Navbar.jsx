69b64365a5d66445f7d8437bc98b9b95ec09b0ff
Alishbaprograms <ash.k1740@gmail.com>
2023-11-25 21:34:25 +0500

    elements

--- frontend/my-app/src/Componenets/Navbar/Navbar.jsx
+++ frontend/my-app/src/Componenets/Navbar/Navbar.jsx
@@ -16,14 +16,14 @@ export const Navbar = () => {
             <p>Voltrium</p>
         </div>
         <ul className="nav-menu">
-            <li onClick ={()=>{setMenu("shop")}}><Link to='/'>Shop</Link>{menu==="shop" ? <hr/>: <></>}</li>
-            <li onClick ={()=>{setMenu("arduino")}}> <Link to='/arduino'>Arduino</Link>{menu==="arduino" ? <hr/>: <></>}</li>
-            <li onClick ={()=>{setMenu("suppliers")}}><Link to='/suppliers'>Suppliers</Link> {menu==="suppliers" ? <hr/>: <></>} </li>
-            <li onClick ={()=>{setMenu("components")}}><Link to='/components'>Components</Link> {menu==="components" ? <hr/>: <></>} </li>
+            <li onClick ={()=>{setMenu("shop")}}><Link style={{textDecoration: 'none'}} to='/'>Shop</Link>{menu==="shop" ? <hr/>: <></>}</li>
+            <li onClick ={()=>{setMenu("arduino")}}> <Link style={{textDecoration: 'none'}} to='/arduino'>Arduino</Link>{menu==="arduino" ? <hr/>: <></>}</li>
+            <li onClick ={()=>{setMenu("suppliers")}}><Link style={{textDecoration: 'none'}} to='/suppliers'>Suppliers</Link> {menu==="suppliers" ? <hr/>: <></>} </li>
+            <li onClick ={()=>{setMenu("components")}}><Link style={{textDecoration: 'none'}} to='/components'>Components</Link> {menu==="components" ? <hr/>: <></>} </li>
         </ul>
         <div className="nav-login-cart">
-            <button>Login</button>
-            <img src={cart_icon} alt="" />
+            <Link to='/login'><button>Login</button></Link>
+            <Link to='/cart'><img src={cart_icon} alt="" /></Link> 
             <div className="nav-cart-count">0</div>
         </div>
     </div>
--- frontend/my-app/src/Componenets/Pages/Shop.jsx
+++ frontend/my-app/src/Componenets/Pages/Shop.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../Hero/Hero'
 
 const Shop = () => {
   return (
-    <div></div>
+    <div>
+        <Hero/>
+    </div>
   )
 }
 
